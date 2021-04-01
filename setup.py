fec763d62e446acf652bc57ad11cb4d1e494d640
tchen <tchen0584@gmail.com>
2021-04-01 10:15:43 -0400

    Production build.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "swbs",
-    version = "1.2.2",
+    version = "1.2.3",
     author = "perpetualCreations",
     author_email = "tchen0584@gmail.com",
     description = "Socket wrapper for sending and receiving byte strings.",

d3f47d88522061b88e7885af5072d129b9d9acae
tchen <tchen0584@gmail.com>
2021-05-25 09:17:46 -0400

    Version jump to exit beta tags, fix wheel build?

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="swbs",
-    version="1.2.5b2",
+    version="1.2.6",
     author="perpetualCreations",
     author_email="tchen0584@gmail.com",
     description="Socket wrapper for sending and receiving byte strings.",
--- source/conf.py
+++ source/conf.py
@@ -23,8 +23,8 @@ copyright = 'MIT License, perpetualCreations'
 author = 'perpetualCreations'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.5b2'
-version = '1.2.5b2'
+release = '1.2.6'
+version = '1.2.6'
 
 
 # -- General configuration ---------------------------------------------------

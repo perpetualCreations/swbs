d9d0b53b987cfc1a52864e15ab9ddce91e624768
tchen <tchen0584@gmail.com>
2021-06-07 09:02:35 -0400

    Change Sphinx builder and theme.

--- jankbuild.bat
+++ jankbuild.bat
@@ -1,2 +1,1 @@
-echo "reminder: please migrate dev work onto a linux machine."
-D:\Python\Scripts\sphinx-build.exe source "F:\@dist_dreamerslegacy\dreamerslegacy-site\projects\swbs\docs"
+sphinx-build source "F:\@dist_dreamerslegacy\dreamerslegacy-site\projects\swbs\docs"
